bd1888751b6fe65da9692638f2783b46d1ff8c44
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-11-13 11:36:22 -0300

    Exclui arquivo imprime_cabecalho.cpp, já que a função inline foi transferida para o cabeçalho.

--- JogoForca/imprime_cabecalho.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "imprime_cabecalho.hpp"
-
-inline void imprime_cabecalho() {
-    std::cout << "*********************" << std::endl;
-    std::cout << "*** Jogo da Forca ***" << std::endl;
-    std::cout << "*********************" << std::endl;
-    std::cout << std::endl;
-}
